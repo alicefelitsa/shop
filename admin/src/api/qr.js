07c9b0cea7841a52c0c039e7b4cc14ddef0cf980
alicefelitsa <223249910@qq.com>
2026-08-15 01:29:18 +0800

    init

--- admin/src/api/qr.js
+++
@@ -1,9 +0,0 @@
-import request from '@/api/request';
-
-export async function getQrcodeUrl(ucode) {
-    const res = await request.get('/getQrcodeUrl?ucode=' + ucode);
-    if (res.data.code === 0) {
-        return res.data.data;
-    }
-    return Promise.reject(new Error(res.data.message));
-}
